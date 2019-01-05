b20113f97a21d030020105b7f81d7735b8433914
William Wong <compulim@hotmail.com>
2019-01-05 00:55:43 -0800

    Clean up

--- packages/app/src/redux/createStore.js
+++ packages/app/src/redux/createStore.js
@@ -18,12 +18,7 @@ export default function () {
       thunk,
       createClockMiddleware(),
       createGeolocationMiddleware(),
-      createHeadingMiddleware(),
-      // () => next => action => {
-      //   console.log(action);
-
-      //   return next(action);
-      // }
+      createHeadingMiddleware()
     )
   );
 
--- packages/app/src/redux/saga.js
+++
@@ -1,9 +0,0 @@
-// import { fork } from 'redux-saga/effects';
-
-// import checkConnection from './sagas/checkConnection';
-// import connect from './sagas/connect';
-
-// export default function* () {
-//   yield fork(checkConnection);
-//   yield fork(connect);
-// }
